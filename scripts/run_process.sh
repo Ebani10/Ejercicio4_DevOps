e462f7a55aefc494520e1f32a9752a8891987b61
Global Name <e.dorofranco@gmail.com>
2026-04-19 14:08:52 -0600

    Update de del runprocess.sh
    Da error al conectar con la ec2, y parece que el archivo de resultados no existe.
    Lo que lleva a concluir que la EC2 no esta ejecutando el programa de python, lo que nos lleva a usar este archivo.

--- scripts/run_process.sh
+++ scripts/run_process.sh
@@ -1,1 +1,8 @@
-python3 /var/www/html/procesar.py
+#!/bin/bash
+# Ir a la carpeta donde están los archivos
+cd /var/www/html/
+# Ejecutar el script de Python para generar el resultado.txt localmente
+python3 procesar.py
+# Asegurar permisos para que Apache pueda leerlo
+chown ec2-user:apache /var/www/html/resultado.txt
+chmod 644 /var/www/html/resultado.txt
